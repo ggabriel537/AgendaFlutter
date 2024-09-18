0bc894beb9bb3c9bf58f498d494cfb5434b4aacc
Gabriel <ggabriel667@gmail.com>
2024-09-18 08:38:21 -0400

    Implementado trabalho em sala de exemplo para se basear nele.

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,155 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Principal(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Cadastro extends StatefulWidget {
+  final CarrosRepository carros;
+  Cadastro({required this.carros});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Cadastro> createState() => _CadastroState(carros: carros);
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CadastroState extends State<Cadastro> {
+  final TextEditingController nomeController = TextEditingController();
+  final TextEditingController corController = TextEditingController();
+  final CarrosRepository carros;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  _CadastroState({required this.carros});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Cadastro de Carros'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Entre com nome'),
+              controller: nomeController,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            TextField(
+              decoration: InputDecoration(labelText: 'Entre com cor'),
+              controller: corController,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  carros.addCarros(Carro(
+                      nome: nomeController.text, cor: corController.text));
+                });
+                Navigator.pop(context);
+              },
+              child: Text('Salvar'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class Principal extends StatelessWidget {
+  final CarrosRepository carros = CarrosRepository();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Principal'),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Cadastro(carros: carros),
+                ),
+              );
+            },
+            child: Text("Cadastro"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Listagem(carros: carros),
+                ),
+              );
+            },
+            child: Text("Listar"),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Listagem extends StatefulWidget {
+  final CarrosRepository carros;
+  Listagem({required this.carros});
+
+  @override
+  State<Listagem> createState() => ListagemState(carros: carros);
+}
+
+class ListagemState extends State<Listagem> {
+  final CarrosRepository carros;
+
+  ListagemState({required this.carros});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Listagem de Carros'),
+      ),
+      body: ListView.builder(
+        itemCount: carros.getCarros().length,
+        itemBuilder: (context, index) {
+          Carro c = carros.getCarros()[index];
+          return ListTile(
+            title: Text(c.nome),
+            subtitle: Text(c.cor),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class Carro {
+  final String nome;
+  final String cor;
+  Carro({required this.nome, required this.cor});
+}
+
+class CarrosRepository {
+  final List<Carro> carros = [];
+
+  void addCarros(Carro c) {
+    carros.add(c);
+  }
+
+  List<Carro> getCarros() {
+    return carros;
   }
 }
